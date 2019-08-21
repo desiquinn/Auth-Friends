551859046460dd47ef71ee35133e98ac63ca878d
Desiree Morris <desiquinncompany@gmail.com>
2019-08-21 10:38:17 -0400

    formatted the FriendForm component

--- auth-friends/src/components/FriendForm.js
+++ auth-friends/src/components/FriendForm.js
@@ -6,3 +6,37 @@ import React from 'react';
 //     age: 24,
 //     email: 'joe@lambdaschool.com',
 //   }
+
+const FriendForm = () => {
+    
+    return(
+        <form>
+            <h1>Add A Friend!</h1>
+            <p>Fill out the form below to add a friend to your friends list.</p>
+            <input 
+                type='text'
+                name='name'
+                placeholder='Name'
+
+
+            />
+            <input 
+                type='number'
+                name='age'
+                placeholder='Age'
+
+                
+            />
+            <input 
+                type='email'
+                name='email'
+                placeholder='Email'
+  
+                  
+            />
+            <button type='submit'>Submit</button>
+        </form>
+    );
+};
+
+export default FriendForm;

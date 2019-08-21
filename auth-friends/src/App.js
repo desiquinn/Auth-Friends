670bf95cbceceaac2a9bc4b53467af2575359085
Desiree Morris <desiquinncompany@gmail.com>
2019-08-21 10:14:31 -0400

    formatted the login component and routes

--- auth-friends/src/App.js
+++ auth-friends/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {Route, Link} from 'react-router-dom';
+
+import Login from './components/Login.js';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+
+
+      {/* {Links} */}
+      <Link to='/login/'>Login</Link>
+
+
+      {/* {Routes} */}
+      <Route path='/login/' component={Login} />
     </div>
   );
 }
--- auth-friends/src/components/Login.js
+++ auth-friends/src/components/Login.js
@@ -1,1 +1,31 @@
+import React from 'react';
+
+
 //username, password, submit button
+
+const Login = () => {
+
+    return(
+        <form>
+            <h1>Login Here!</h1>
+            <p> Enter your username and password to gain access to your friends list</p>
+            <input 
+                type='text'
+                name='username'
+                placeholder='Username'
+
+
+            />
+            <input 
+                type='password'
+                name='password'
+                placeholder='Password'
+
+                
+            />
+            <button type='submit'>Login</button>
+        </form>
+    )
+}
+
+export default Login;
